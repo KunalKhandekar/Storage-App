8e3d7dfe2d006b5097cac14afe569cffe6bf91cc
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-06-17 13:52:44 +0530

    Minor changes

--- Server/middlewares/validateRequest.js
+++ Server/middlewares/validateRequest.js
@@ -1,7 +1,7 @@
-import { ObjectId } from "mongodb";
+import { Types } from "mongoose";
 
 export default function validateRequest(req, res, next, id) {
-    if (!ObjectId.isValid(id)) {
+    if (!Types.ObjectId.isValid(id)) {
         return res.status(401).json({
             error: `Invalid ID: ${id}`,
         })
