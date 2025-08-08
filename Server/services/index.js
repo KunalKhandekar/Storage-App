bf17ee268254f2341e3866ab274fffdbf57041e5
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-08-08 17:28:27 +0530

    User Services Done

--- Server/services/index.js
+++ Server/services/index.js
@@ -1,3 +1,3 @@
 export { default as AuthServices } from "./auth/index.js";
 export { default as DirectoryServices } from "./Directory/index.js";
-
+export { default as UserServices } from "./user/index.js";
