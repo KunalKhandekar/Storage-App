faffdaae50b7dea66f157cad69cb9cb26fe5b934
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-08-09 14:07:48 +0530

    File Services Done

---
+++ Server/services/file/index.js
@@ -0,0 +1,289 @@
+import { StatusCodes } from "http-status-codes";
+import { rm } from "node:fs/promises";
+import path from "node:path";
+import { absolutePath } from "../../app.js";
+import Directory from "../../models/dirModel.js";
+import File from "../../models/fileModel.js";
+import User from "../../models/userModel.js";
+import CustomError from "../../utils/ErrorResponse.js";
+
+const uploadFileService = async (file, parentDirId, userId) => {
+  if (!file) {
+    throw new CustomError("No files uploaded", StatusCodess.BAD_REQUEST);
+  }
+  const { originalname, storedName } = file;
+
+  const parentDirectory = await Directory.findOne({
+    _id: parentDirId,
+    userId,
+  }).lean();
+
+  if (!parentDirectory) {
+    await rm(path.join(absolutePath, storedName));
+    throw new CustomError(
+      "You are not authorized to make this action",
+      StatusCodes.UNAUTHORIZED
+    );
+  }
+
+  const newFile = await File.create({
+    storedName,
+    userId,
+    name: originalname,
+    parentDirId: parentDirectory._id,
+  });
+
+  return newFile;
+};
+
+const getFileService = async (id, userId) => {
+  const file = await File.findOne({
+    _id: id,
+    userId,
+  });
+
+  if (!file) {
+    throw new CustomError("File not found", StatusCodes.NOT_FOUND);
+  }
+
+  return file;
+};
+
+const renameFileService = async (id, userId, name) => {
+  const file = await File.findOne({
+    _id: id,
+    userId,
+  }).lean();
+
+  if (!file) {
+    throw new CustomError("File not found", StatusCodes.NOT_FOUND);
+  }
+  await File.updateOne({ _id: file._id }, { $set: { name } }).lean();
+
+  return file;
+};
+
+const deleteFileService = async (id, userId) => {
+  const file = await File.findOne({
+    _id: id,
+    userId,
+  });
+
+  if (!file) {
+    throw new CustomError("File not found", StatusCodes.NOT_FOUND);
+  }
+  await File.deleteOne({ _id: file._id });
+  if (!file.googleFileId) {
+    await rm(path.join(absolutePath, file.storedName));
+  }
+
+  return file;
+};
+
+const shareViaEmailService = async (users, file) => {
+  let response = [];
+  for (const { email, name, permission } of users) {
+    const user = await User.findOne({ email }).lean();
+
+    if (!user) {
+      response.push(
+        `${name} -> ${email} is not registered, file cannot be shared.`
+      );
+      continue;
+    }
+
+    const alreadyShared = file.sharedWith.find(
+      (u) => u.userId.toString() === user._id.toString()
+    );
+
+    if (alreadyShared) {
+      response.push(`${name} -> File already shared with ${email}.`);
+      continue;
+    }
+
+    file.sharedWith.push({
+      permission,
+      userId: user._id,
+    });
+  }
+  await file.save();
+
+  return response;
+};
+
+const shareviaLinkService = async (file, permission) => {
+  if (file?.sharedViaLink?.token) {
+    return {
+      link: `${process.env.CLIENT_URL}/guest/access/${file._id}?token=${file.sharedViaLink.token}`,
+      permission: file.sharedViaLink.permission,
+      enabled: file.sharedViaLink.enabled,
+    };
+  }
+
+  file.sharedViaLink = {
+    enabled: false,
+    permission,
+    token: crypto.randomUUID(),
+  };
+
+  await file.save();
+
+  return {
+    link: `${process.env.CLIENT_URL}/guest/access/${file._id}?token=${file.sharedViaLink.token}`,
+    permission: file.sharedViaLink.permission,
+    enabled: file.sharedViaLink.enabled,
+  };
+};
+
+const shareLinkToggleService = async (file, enabled) => {
+  file.sharedViaLink.enabled = enabled;
+  await file.save();
+
+  return file.sharedViaLink.permission;
+};
+
+const getSharedFileViaLinkService = async (fileId, token) => {
+  const file = await File.findById(fileId).lean();
+  if (!file) {
+    throw new CustomError("File not found", StatusCodes.NOT_FOUND);
+  }
+
+  if (!file.sharedViaLink.enabled) {
+    throw new CustomError(
+      "File access has been disabled by the user.",
+      StatusCodes.CONFLICT
+    );
+  }
+
+  if (file.sharedViaLink.token !== token) {
+    throw new CustomError("File Access token is Invalid", StatusCodes.CONFLICT);
+  }
+
+  return file;
+};
+
+const getFileInfoService = async (fileId) => {
+  const file = await File.findById(fileId)
+    .select("name sharedViaLink userId")
+    .populate("userId");
+
+  const url = `${process.env.BASE_URL}/guest/file/view/${file._id}?token=${file.sharedViaLink.token}`;
+
+  return {
+    url,
+    name: file.name,
+    sharedBy: file.userId.name,
+    isAccessible: file.sharedViaLink.enabled,
+  };
+};
+
+const getSharedFileViaEmailService = async (fileId, userId) => {
+  const file = await File.findById(fileId).lean();
+  if (!file) {
+    throw new CustomError("File not found", StatusCodes.NOT_FOUND);
+  }
+
+  const hasAccess = file.sharedWith.find((u) => u.userId.equals(userId));
+
+  if (!hasAccess) {
+    throw new CustomError(
+      "You are not authorized to access this file.",
+      StatusCodes.UNAUTHORIZED
+    );
+  }
+
+  return file;
+};
+
+const changeFileSharePermissionService = async (file, permission) => {
+  file.sharedViaLink.permission = permission;
+  await file.save();
+
+  return file.sharedViaLink.permission;
+};
+
+const changePermissionOfUserService = async (file, permission, userId) => {
+  // Checking if the given user exist in the shared list.
+  const collaborator = file.sharedWith.find(
+    (c) => c.userId._id.toString() === userId
+  );
+  if (!collaborator) {
+    throw new CustomError(
+      "File is not shared with the selected user.",
+      StatusCodes.CONFLICT
+    );
+  }
+
+  collaborator.permission = permission;
+  await file.save();
+
+  return collaborator.permission;
+};
+
+const revokeUserAccessService = async (file, userId) => {
+  // Checking if the given user exist in the list.
+  const collaborator = file.sharedWith.find(
+    (c) => c.userId._id.toString() === userId
+  );
+  if (!collaborator) {
+    throw new CustomError(
+      "File is not shared with the selected user.",
+      StatusCodes.CONFLICT
+    );
+  }
+
+  file.sharedWith = file.sharedWith.filter(
+    (c) => c.userId._id.toString() !== userId
+  );
+  await file.save();
+};
+
+const getUserAccessListService = async (fileId, userId) => {
+  const sharedUsers =
+    (
+      await File.findById(fileId)
+        .populate("sharedWith.userId", "name email picture")
+        .select("sharedWith.userId sharedWith.permission sharedWith.sharedAt")
+        .lean()
+    )?.sharedWith || [];
+
+  const allUsers = (
+    await User.find().select("name email picture").lean()
+  ).filter((u) => !userId.equals(u._id));
+
+  const sharedUserIdArray = sharedUsers.map((u) => u.userId._id.toString());
+
+  const availableUsers = allUsers.filter(
+    (u) => !sharedUserIdArray.includes(u._id.toString())
+  );
+
+  return {
+    sharedUsers,
+    availableUsers,
+  };
+};
+
+const renameFileByEditorService = async (file, name) => {
+  file.name = name;
+  await file.save();
+
+  return file.name;
+};
+
+export default {
+  UploadFileService: uploadFileService,
+  GetFileService: getFileService,
+  RenameFileService: renameFileService,
+  DeleteFileService: deleteFileService,
+  ShareViaEmailService: shareViaEmailService,
+  ShareviaLinkService: shareviaLinkService,
+  ShareLinkToggleService: shareLinkToggleService,
+  GetSharedFileViaLinkService: getSharedFileViaLinkService,
+  GetFileInfoService: getFileInfoService,
+  GetSharedFileViaEmailService: getSharedFileViaEmailService,
+  ChangeFileSharePermissionService: changeFileSharePermissionService,
+  ChangePermissionOfUserService: changePermissionOfUserService,
+  RevokeUserAccessService: revokeUserAccessService,
+  GetUserAccessListService: getUserAccessListService,
+  RenameFileByEditorService: renameFileByEditorService 
+};
