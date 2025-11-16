87e3286ba030b6d618dcf2ba4395733288e62277
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-11-16 14:34:44 +0530

    cloudfront expiry time fixed

--- Server/services/file/cloudFront.js
+++ Server/services/file/cloudFront.js
@@ -3,18 +3,23 @@ import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
 const cloudfrontDistributionDomain = process.env.CLOUDFRONT_URL;
 const privateKey = process.env.CLOUDFRONT_PRIVATE_KEY;
 const keyPairId = process.env.KEY_PAIR_ID;
-const dateLessThan = new Date(Date.now() + 1000 * 60 * 60);
 
-export const getCloudFrontSignedURL = ({ File, Action }) => {
+export const getCloudFrontSignedURL = ({ File, Action, expiresIn = 3600 }) => {
   const fileName = File.name;
   let Key = File.originalKey;
+
   if (Action !== "download" && File.googleFileId && File.pdfKey) {
     Key = File.pdfKey;
   }
+
   const encodedDisposition = encodeURIComponent(
     `${Action === "download" ? "attachment" : "inline"}; filename="${fileName}"`
   );
+
   const url = `${cloudfrontDistributionDomain}/${Key}?response-content-disposition=${encodedDisposition}`;
+
+  const dateLessThan = new Date(Date.now() + expiresIn * 1000);
+
   return getSignedUrl({
     url,
     keyPairId,
