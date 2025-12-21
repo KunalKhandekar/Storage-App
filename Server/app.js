9a29ec31dcb63fc8764d064d07b8645ea05e9845
Sahil Khandekar <134169718+KunalKhandekar@users.noreply.github.com>
2025-12-21 16:31:18 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -104,7 +104,7 @@ app.use("/events", eventController);
 app.use("/verify-subscription", verifySubscriptionId);
 
 app.get("/", (req, res) => {
-  return CustomSuccess.send(res, "App working fine in production", StatusCodes.OK);
+  return CustomSuccess.send(res, "App working fine in production environment", StatusCodes.OK);
 });
 
 // Error Handler
